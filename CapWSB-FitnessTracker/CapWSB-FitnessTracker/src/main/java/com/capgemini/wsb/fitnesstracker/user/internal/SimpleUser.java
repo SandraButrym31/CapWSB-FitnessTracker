9f17166eaad3f7bf836048d83939b2c6775e5198
sander3105 <122493224+SandraButrym31@users.noreply.github.com>
2024-06-16 23:13:35 +0200

    Delete CapWSB-FitnessTracker directory

--- CapWSB-FitnessTracker/CapWSB-FitnessTracker/src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/SimpleUser.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.wsb.fitnesstracker.user.internal;
-
-import jakarta.annotation.Nullable;
-
-public record SimpleUser(@Nullable Long id, String firstName, String lastName) {
-
-}
